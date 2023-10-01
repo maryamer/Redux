0e7e0a79f7e79a0048cbc5380a72dae12d49cd9d
maryameb <personalmaryamebrahimi@gmail.com>
2023-10-02 03:22:13 +0330

    Async-action-useReducer

---
+++ src/redux/user/userReducer.js
@@ -0,0 +1,40 @@
+import {
+  FETCH_USERS_FAILURE,
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS,
+} from "./userTypes";
+
+const initialState = {
+  loading: false,
+  data: [],
+  error: "",
+};
+export default function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case FETCH_USERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+      };
+    case FETCH_USERS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        data: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+// fetch user is an Async action (side effect)=> redux-thunk
+// 1.request
+// 2.success
+// 3.failure
